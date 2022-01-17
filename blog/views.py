a50ad84091f47903b0b7fc1e15c4ad5651a4fec0
evgeniya35 <evgeniya35@yandex.ru>
2022-01-17 13:27:55 +0300

    change related_name post_comment to comments

--- blog/views.py
+++ blog/views.py
@@ -27,7 +27,7 @@ def serialize_post_optimized(post):
         'title': post.title,
         'teaser_text': post.text[:200],
         'author': post.author.username,
-        'comments_amount': post.post_comment.count(),
+        'comments_amount': post.comments_count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
@@ -63,15 +63,16 @@ def index(request):
     most_popular_posts_ids = [post.id for post in most_popular_posts]
     
     posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids) \
-        .annotate(comments_count=Count('post_comment'))
+        .annotate(comments_count=Count('comments'))
     ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
     count_for_id = dict(ids_and_comments) 
 
     for post in most_popular_posts:
         post.comments_count = count_for_id[post.id]
 
-    most_fresh_posts = Post.objects.order_by('-published_at') \
-        .prefetch_related('author', 'post_comment')[:5]
+    most_fresh_posts = Post.objects.annotate(comments_count=Count('comments')) \
+        .order_by('-published_at') \
+        .prefetch_related('author')[:5]
 
     most_popular_tags = Tag.objects.annotate(Count('posts')) \
         .order_by('-posts__count')[:5]
